9baa689be5590f1a526d4f6a26ec37dde469155f
Fiorella Quino <dkafio@gmail.com>
2019-01-20 10:55:31 -0500

    api users

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
   ## API
   namespace :api do
     namespace :v1 do
-     resources :purchases
+      resources :users
+      resources :purchases
     end
   end
 
