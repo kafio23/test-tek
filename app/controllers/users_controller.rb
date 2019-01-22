fc8f68ff125cad28711600c590cf38044961d34f
Fiorella Quino <dkafio@gmail.com>
2019-01-21 21:54:31 -0500

    cambios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:show]
+	before_action :set_user, only: [:show, :edit, :update]
 
 	def index
 		@users = User.all
@@ -22,10 +22,21 @@ class UsersController < ApplicationController
     end
 	end
 
+	def edit
+	end
+
+	def update
+		if @user.update user_params
+			redirect_to users_path, notice: "El Usuario #{@user.full_name} se modificó correctamente."
+		else
+			render :edit, alert: 'Error en:'
+		end
+	end
+
 	private
 		def user_params
 			# policy(@user).permitted_attributes
-			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+			params.require(:user).permit(:full_name, :email, :password, :password_confirmation)
 		end
 		def set_user
 			@user = User.find(params[:id])
