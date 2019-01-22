f734dd20ab969aa6c9092aeb9914789b82f7fab4
Fiorella Quino <dkafio@gmail.com>
2019-01-21 22:00:17 -0500

    devices model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_19_181639) do
+ActiveRecord::Schema.define(version: 2019_01_22_025732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "devices", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "device_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "purchases", force: :cascade do |t|
     t.text "description"
